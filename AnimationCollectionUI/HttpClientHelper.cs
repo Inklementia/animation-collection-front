ce4e00de87b200223ac737d9444fe5b22d2c5cba
Inklementia <inna2001.a@gmail.com>
2021-11-09 18:29:23 +0500

    update api link

--- AnimationCollectionUI/HttpClientHelper.cs
+++ AnimationCollectionUI/HttpClientHelper.cs
@@ -11,7 +11,7 @@ namespace AnimationCollectionUI
     {
         public static HttpClient GetHttpClient()
         {
-            string Baseurl = "http://ec2-18-224-43-71.us-east-2.compute.amazonaws.com/";
+            string Baseurl = "http://ec2-18-191-154-153.us-east-2.compute.amazonaws.com/";
 
             //initializing new HttpClient
             var client = new HttpClient();
